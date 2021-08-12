c1c469486c90114f051fc53fe16d2eb31dbff388
Jan Warchoł <jan.warchol@gmail.com>
2021-08-12 13:01:04 +0200

    Try being smart, doesn't work

--- install.sh
+++ install.sh
@@ -7,8 +7,16 @@ if [[ ! "$PATH" == *$REPO_PATH* ]]; then
     echo -e "\nexport PATH=\"\$PATH:$REPO_PATH\"" >> ~/.bashrc;
 fi
 
-# add a hook for replacing regular git prompt with recursive one
-echo 'export PS1=$(echo "$PS1" | sed "s/__git_ps1/__git_recursive_ps1/")' >> ~/.bashrc
+# ensure bash prompt shows smart git status
+if [[ ! "$PS1" == *__git_ps1_recursive* ]]; then
+    if [[ "$PS1" == *__git_ps1* ]]; then
+        # add a hook for replacing regular git prompt with recursive one
+        echo 'export PS1=$(echo "$PS1" | sed "s/__git_ps1_recursive/__git_ps1/")' >> ~/.bashrc
+    else
+        # no git status in prompt at all - override prompt
+        echo 'export PS1="\[\e[94m\]\u@\h \[\e[96m\]\w\[\e[0m\]\$(__git_ps1_recursive)\n\\$ "' >> ~/.bashrc
+    fi
+fi
 
 # configure alias for smart status
 git config --global alias.st smart-status
